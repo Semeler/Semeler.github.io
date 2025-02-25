5eb455ca23b055002b40ddef8b5cb433ed1e0fb6
semeler <lucasemeler@gmail.com>
2025-02-24 21:05:54 -0400

    Publicando portfólio no GitHub Pages principal

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/semeler.github.io/', // Altere para o nome exato do repositório no GitHub
+  base: '/', // Sem repositório no caminho
 });
